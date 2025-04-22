97348a01b66654b667208d8da9315d39f3e4e9df
charanbuddhavarapu <charansri10@gmail.com>
2025-04-22 11:38:45 -0400

    Day2_window functions and objects explanation

---
+++ FrontEnd/day_02/demo_2.js
@@ -0,0 +1,13 @@
+let a = 2;
+let b = 3;
+
+//ES6
+console.log(`Addition of ${a} and ${b} is ${a+b}`)
+
+
+//string Concatenation
+//console.log(`product of `+a +` and `+b+` is `+{a+b}``); 
+
+window.alert(`This is a demo alert and works only in browser`);
+
+console.log(`This is a demo alert and works only in browser`);

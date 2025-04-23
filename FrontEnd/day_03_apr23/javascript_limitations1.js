c9c19578907d4ed034e44143c5239780a98fc533
charanbuddhavarapu <charansri10@gmail.com>
2025-04-23 14:22:36 -0400

    JS_objectTypes along with typescript initialization

---
+++ FrontEnd/day_03_apr23/javascript_limitations1.js
@@ -0,0 +1,10 @@
+let a = 10;
+console.log(a) //10
+
+//but if you assign a = sachin, then also it will work
+//in JS, we can declare a variable and then assign any kind of value to it, which is not good
+
+a = 'rohit'
+console.log(a); // it gives a value as sachin
+
+// but in type script we need to mention the type of variable ,so we cannot have any other value other than that
